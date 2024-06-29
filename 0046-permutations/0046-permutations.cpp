7c6f8ef8a68e935cfc9b8478780c806e5b04c4c3
Mohit Kumawat <159867125+Mohit-Kumawat2004@users.noreply.github.com>
2024-06-29 08:48:54 +0430

    Time: 3 ms (54.06%), Space: 10.1 MB (23.87%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,24 +1,23 @@
 class Solution {
-public:
-    void rec(int idx,vector<int>&nums,vector<vector<int>>&ans){
-        //if our index reached nums.size() then we will and the nums in ans and return
-        if(idx==nums.size()){
+private:
+    void solve(vector<int> nums, vector<vector<int>> &ans,int index){
+        //base case
+        if(index >= nums.size()){
             ans.push_back(nums);
             return;
         }
-        //we are traversing from idx to nums.size() and swapping the nums[i] and nums[idx]
-        for(int i=idx;i<nums.size();i++){
-            swap(nums[idx],nums[i]);
-            rec(idx+1,nums,ans);
-            //backtracking 
-            swap(nums[idx],nums[i]);
+        
+        for(int j=index;j<nums.size();j++){
+            swap(nums[index],nums[j]);
+            solve(nums,ans,index+1);
+            swap(nums[index],nums[j]);
         }
     }
+public:
     vector<vector<int>> permute(vector<int>& nums) {
-        //we have created the ans vector for storing all the permutation in it
-        vector<vector<int>>ans;
-        //we are calling recursive function for the 0th index
-        rec(0,nums,ans);
+        vector<vector<int>> ans;
+        int index=0;
+        solve(nums,ans,index);
         return ans;
     }
 };
