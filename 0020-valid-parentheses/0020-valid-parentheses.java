5446f143aa5f62a44b4944afb2e58b03c703a549
Mohit Kumawat <159867125+Mohit-Kumawat2004@users.noreply.github.com>
2025-08-06 13:33:46 +0530

    Update 0020-valid-parentheses.java

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -6,11 +6,11 @@ class Solution {
 			stack.push(')');
 		else if (c == '{')
 			stack.push('}');
-		else if (c == '[')
+		else if (c == '[') 
 			stack.push(']');
 		else if (stack.isEmpty() || stack.pop() != c)
-			return false;
-	}
+			return false; 
+	} 
 	return stack.isEmpty();
     }
 }
