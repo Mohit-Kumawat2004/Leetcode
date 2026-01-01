e9d7ca6c58aadf1216f616f1ec38a91d927d6e1c
Mohit Kumawat <159867125+Mohit-Kumawat2004@users.noreply.github.com>
2026-01-01 12:15:46 +0530

    asdfghjk

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -1,7 +1,4 @@
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        """
-        Applying Two Pointer Approach
-        """
         for i in range(len(s) // 2):     #iterate half of string to get mid point
             s[i], s[-i - 1] = s[-i - 1], s[i]
